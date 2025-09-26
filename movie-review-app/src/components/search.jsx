9c950a56b365d4562495f8b031672e33cd181dc9
Swapnil345 <swapnilkm03@gmail.com>
2025-09-26 20:33:22 +0530

    API integration TMDB

--- movie-review-app/src/components/search.jsx
+++ movie-review-app/src/components/search.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
-const search = () => {
+const search = ({searchTerm,setSearchTerm}) => {
     return (
-    <div className='text-white text-3xl'>
-        Search
+    <div className='search'>
+        <div>
+            <img src="search.svg" alt="search" />
+            <input
+            type="text"
+            placeholder="Search through thousands of movies"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            />
+        </div>
     </div>
     )
 }
